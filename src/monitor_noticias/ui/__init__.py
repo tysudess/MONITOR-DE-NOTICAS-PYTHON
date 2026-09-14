a716d66b2f0d7da1dd7b8bcaa3fb95dd1a5d1b91
tysudess <rodrigodess@yahoo.com.br>
2026-09-14 15:50:28 -0300

    Carrega recursos dinâmicos da tela Início

--- src/monitor_noticias/ui/__init__.py
+++ src/monitor_noticias/ui/__init__.py
@@ -1,1 +1,5 @@
-"""Interface PySide6. Somente fundação mínima neste passo."""
+"""Interface PySide6 do Monitor de Notícias."""
+
+from monitor_noticias.ui.home_runtime_features import install_home_runtime_features
+
+install_home_runtime_features()
