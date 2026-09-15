7c066bdbc40733f172db8923a874f4e7f3e621bc
tysudess <rodrigodess@yahoo.com.br>
2026-09-15 19:35:58 -0300

    Home: ativa ajuste de precisão visual

--- run.py
+++ run.py
@@ -27,6 +27,7 @@ from monitor_noticias.ui.v015_shell_reference import install_v015_shell_referenc
 from monitor_noticias.ui.v016_layout_only import install_v016_layout_only
 from monitor_noticias.ui.v017_home_truth import install_v017_home_truth
 from monitor_noticias.ui.v017_home_truth_refine import install_v017_home_truth_refine
+from monitor_noticias.ui.v017_home_truth_precision import install_v017_home_truth_precision
 
 install_v009_runtime_fixes()
 install_v009_pdf_cover_patch()
@@ -48,6 +49,7 @@ install_v016_layout_only()
 # Motor, controller, rotas e paginas dos demais modulos permanecem intactos.
 install_v017_home_truth()
 install_v017_home_truth_refine()
+install_v017_home_truth_precision()
 
 from monitor_noticias.app.application import main
 
