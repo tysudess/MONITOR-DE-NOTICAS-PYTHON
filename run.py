16715e6d09e8703b4c301f12be4580f459cbef99
tysudess <rodrigodess@yahoo.com.br>
2026-09-15 10:48:56 -0300

    Ativa correções de runtime v0.0.10

--- run.py
+++ run.py
@@ -14,9 +14,11 @@ if "--extractor-worker" in sys.argv:
 
 from monitor_noticias.ui.v009_runtime_fixes import install_v009_runtime_fixes
 from monitor_noticias.ui.v009_pdf_cover_patch import install_v009_pdf_cover_patch
+from monitor_noticias.ui.v010_runtime_fixes import install_v010_runtime_fixes
 
 install_v009_runtime_fixes()
 install_v009_pdf_cover_patch()
+install_v010_runtime_fixes()
 
 from monitor_noticias.app.application import main
 
